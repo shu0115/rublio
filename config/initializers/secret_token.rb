6752dc763ef744e4ed4ca6022a1fe55d616e727a
shu0115 <raisondetre0115@gmail.com>
2012-06-26 15:09:38 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rublio::Application.config.secret_token = 'abe3b826366b006210c5f937b040be786e83c5a5e75e680e246bb28db4fc3424688c200362bec0a0354e69e5ae08f3b93c41c4f32911411bd89c5cdef6db8f20'
