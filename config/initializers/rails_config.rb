287878c9f7a49f6de945c400133128cbcb23dfc9
shu0115 <s.matsumoto0115@gmail.com>
2016-05-11 14:11:25 +0900

    Update rails_config

--- config/initializers/rails_config.rb
+++
@@ -1,6 +0,0 @@
-RailsConfig.setup do |config|
-  config.const_name = "Settings"
-
-  # ENV['TWITTER_KEY'] = "DUVcmSj94mFlTgoUNVw3g"
-  # ENV['TWITTER_SECRET'] = "6UsrXUMN01Ql7lJUzq0VIt7DmMEA4gFoaR1WQjWiO0"
-end
