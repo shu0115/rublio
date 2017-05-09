caac96001520d8f767937223a3e633febe88dca3
shu0115 <s.matsumoto0115@gmail.com>
2017-05-09 15:19:22 +0900

    Add arel_table matches

--- app/controllers/my_controller.rb
+++ app/controllers/my_controller.rb
@@ -1,8 +1,9 @@
 class MyController < ApplicationController
   def library(controller: nil, action: nil, group_name: nil)
+    @group_name = group_name
     @active = { "#{controller}_#{action}" => "active" }
     @groups = Group.where(user_id: current_user.id).order('groups.name ASC').references(:pages)
-    @groups.where!("groups.name LIKE :name", name: "%#{group_name}%") if group_name.present?
+    @groups.where!(Group.arel_table[:name].matches("%#{@group_name}%")) if @group_name.present?
     @group  = Group.new
   end
 
@@ -13,11 +14,11 @@ class MyController < ApplicationController
 
     # 半角スペースor全角スペースで分割する
     word.split(/ |　/).each do |w|
-      # 大文字小文字区別なしAND検索
+      # 大文字小文字区別なし検索(arel_table.matches)
       if title.present?
-        @pages.where!("pages.title LIKE :word", word: "%#{w}%")
+        @pages.where!(Page.arel_table[:title].matches("%#{w}%"))
       elsif content.present?
-        @pages.where!("pages.content LIKE :word", word: "%#{w}%")
+        @pages.where!(Page.arel_table[:content].matches("%#{w}%"))
       end
     end
   end
