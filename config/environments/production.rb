3255f275b74f0b333bf788a17db7ea48fa3db8f1
shu0115 <s.matsumoto0115@gmail.com>
2018-11-28 16:45:10 +0900

    Fix setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,13 +85,13 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # SendGrid
-  config.action_mailer.default_url_options = { host: 'calem.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rublio.herokuapp.com' }
   config.action_mailer.delivery_method     = :smtp
   config.action_mailer.perform_deliveries  = true
   config.action_mailer.smtp_settings = {
     user_name:            ENV['SENDGRID_USERNAME'],
     password:             ENV['SENDGRID_PASSWORD'],
-    domain:               'calem.herokuapp.com',
+    domain:               'rublio.herokuapp.com',
     address:              'smtp.sendgrid.net',
     port:                 587,
     authentication:       :plain,
